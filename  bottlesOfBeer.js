704061dbde7b90b40cc72ed6fb3b22630a0d1933
Samim Kamleyar <ahmadsamimkamelyar@gmail.com>
2020-05-26 15:59:00 -0700

    bottlesOFBeers

---
+++  bottlesOfBeer.js
@@ -0,0 +1,6 @@
+for (let i = 100; i >= 0; i--) {
+  if (i != 100) {
+    console.log(`Take one down and pass it around ${i} bottles of beer on the wall\n`)
+  }
+  console.log(`${i} bottles of beer on the wall ${i} bottles of beer\n`)
+}
