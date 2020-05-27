15a2f3e797fb30abb1d102374d250dde7f8ed7f8
Samim Kamleyar <ahmadsamimkamelyar@gmail.com>
2020-05-27 11:57:05 -0700

    bottleOfBeers done

---  bottlesOfBeer.js
+++  bottlesOfBeer.js
@@ -3,4 +3,4 @@ for (let i = 100; i >= 0; i--) {
     console.log(`Take one down and pass it around ${i} bottles of beer on the wall\n`)
   }
   console.log(`${i} bottles of beer on the wall ${i} bottles of beer\n`)
-}
+}i
